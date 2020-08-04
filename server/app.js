e3a3e2149d38952283296754919d678f52fc8e72
shizuko <aka-shizuko@hotmail.com>
2020-08-04 16:27:52 -0400

    Connected from express server to mongoDB using mongoose. Also added some server side logging using pino.

--- server/app.js
+++ server/app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const pino = require('pino');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const exit = process.exit;
+// setting up logger
+const l = pino({
+    level: process.env.LOG_LEVEL || 'debug',
+    name: process.env.APP_ID || ''
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +26,24 @@ app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// set up mongoose connection
+mongoose.Promise = global.Promise;
+const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
+mongoose.connect(
+    `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+mongoose.connection.once('open', () => {
+    l.info(`connected to MongoDB via Mongoose`);
+});
+mongoose.connection.on('error', (err) => {
+    l.error(`unable to connect to Mongo via Mongoose`, err);
+    exit(1);
+});
 
 // serve react app
 app.get('*', function(req, res, next) {
