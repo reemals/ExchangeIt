d3b70c5e4a3e78eae9134ccad670b9a7588e8cb3
Illean Zhang <illean@outlook.com>
2020-08-06 16:52:55 -0700

    set up basic routing

---
+++ server/client/src/components/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+    render () {
+        //TODO: replace
+        return <p>dashboard</p>
+    }
+
+}
+
+export default Dashboard;
---
+++ server/client/src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        //TODO: replace
+        return <p>header</p>;
+    }
+
+}
+
+export default Header;
