9fdb342fd9950ad4892c7e66902c1c5007d1bdf8
Meijing Li <lim147@mcmaster.ca>
2020-08-06 10:32:42 -0400

    add alert html

---
+++ script.js
@@ -0,0 +1,12 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function closeFormAndAdd() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("newAlert").style.display = "block";
+}
